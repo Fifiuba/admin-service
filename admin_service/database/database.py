9fe92b6f6be00e6cf6cb65f9611acfbb5904de7d
alejovillores <avillores@fi.uba.ar>
2022-09-14 15:44:18 -0300

    fix: Engine for test

--- admin_service/database/database.py
+++ admin_service/database/database.py
@@ -10,9 +10,8 @@ load_dotenv()
 if "RUN_ENV" in os.environ.keys() and os.environ["RUN_ENV"] == "test":
     # read from .env file, if DATABASE_URL does not exist
     # then read from system env
-    DB_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
     engine = create_engine(
-    DB_URL, echo=True,connect_args={"check_same_thread": False}
+    "sqlite:///./test.db", echo=True,connect_args={"check_same_thread": False}
 )
 else:
     DB_URL = os.getenv("DATABASE_URL")
