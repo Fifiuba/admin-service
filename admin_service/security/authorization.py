282571c0dfed870d0001e46e82f40cf1217adb9b
alejovillores <avillores@fi.uba.ar>
2022-09-20 12:09:53 -0300

    fix: Regex expression

--- admin_service/security/authorization.py
+++ admin_service/security/authorization.py
@@ -1,15 +1,19 @@
 import re
 from admin_service.errors import exceptions
 
-validator = re.compile(r'^Barear\s+') 
+validator = re.compile(r'^(Baerer\s)(.*)') 
 # Matchea que inicie con Barear
 # que luego tenga un espacion(\s)
 # 1 o mas caracteres (token)
 
 def is_auth(headers):
     header =  headers.get('authorization')
+    print(header)
     if header is not None:
         if validator.match(header):
+            print(validator.search(header).groups())
             return True
+        else:
+            raise exceptions.AdminUnauthorized
     else:
         raise exceptions.AdminUnauthorized
