d8ecf8a2ec199a10688b9843da565b95fbd5526a
alejovillores <avillores@fi.uba.ar>
2022-10-11 11:09:22 -0300

    fix: linter

--- admin_service/security/jwt_handler.py
+++ admin_service/security/jwt_handler.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+
+# from datetime import datetime, timedelta
 from jose import jwt
 
 
@@ -25,10 +26,11 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 2
 
 """
 
+
 def create_access_token(admin_id: int, admin: bool) -> str:
-    expires_delta: datetime = datetime.utcnow() + timedelta(
-        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-    )
+    # expires_delta: datetime = datetime.utcnow() + timedelta(
+    #    minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+    # )
     to_encode = {"id": admin_id, "admin": admin}
     encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, ALGORITHM)
     return encoded_jwt
