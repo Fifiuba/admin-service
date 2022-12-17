873f2963a214103bbd2b1ee4470db1a660d581aa
alejovillores <avillores@fi.uba.ar>
2022-12-17 09:11:39 -0300

    feat: add route

--- admin_service/app.py
+++ admin_service/app.py
@@ -13,4 +13,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+@app.get("/")
+def welcome():
+    data = {'service': 'Administrator service!',
+    'created_on':'9-9-2022',
+    'description':'Admin service is the responsable of manage admins'}
+    return data
+
 app.include_router(admin_controller.admin_route, prefix="/admins", tags=["Admins"])
